25b76a10003da9d6887632c2020c1f971b69aa6a
Daniel Gruber <daniel.gruber@altofing.de>
2021-12-01 12:41:57 +0100

    Added hero images

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var faqCollapsibles = document.getElementsByClassName("faq-entry-header");
+var images = document.getElementsByClassName("hero-image");
+let imageChangeInterval = 3000;
+var currentImage = 0;
 
 for (let i = 0; i < faqCollapsibles.length; i++) {
     faqCollapsibles[i].addEventListener("click", function () {
@@ -11,4 +14,16 @@ for (let i = 0; i < faqCollapsibles.length; i++) {
             content.style.display = "block";
         }
     });
+}
+
+setInterval(() => { toggleImage() }, imageChangeInterval);
+toggleImage();
+
+function toggleImage() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    images[currentImage].style.display = "flex";
+
+    currentImage = (currentImage + 1) % images.length;
 }
